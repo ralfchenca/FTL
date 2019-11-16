cb793bc9dbe959468c73e7a68d1ad5215d578500
DL6ER <dl6er@dl6er.de>
2019-11-16 12:25:20 +0100

    Add simple /api/auth implementation. Currently, all passwords are accepted and a login session is valid for 5 minutes.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/FTL.h
+++ src/FTL.h
@@ -102,6 +102,10 @@
 // Default: 1000 (one second)
 #define DATABASE_BUSY_TIMEOUT 1000
 
+// After how much time does a valid API session expire? [seconds]
+// Default: 300 (five minutes)
+#define API_SESSION_EXPIRE 300
+
 // FTLDNS enums
 enum { QUERIES, FORWARDED, CLIENTS, DOMAINS, OVERTIME, WILDCARD };
 enum { DNSSEC_UNSPECIFIED, DNSSEC_SECURE, DNSSEC_INSECURE, DNSSEC_BOGUS, DNSSEC_ABANDONED, DNSSEC_UNKNOWN };
