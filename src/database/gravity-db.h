c83262aa2ce57bebbb785b878dbeca60993f7b2a
DL6ER <dl6er@dl6er.de>
2019-12-02 17:39:00 +0000

    Implement transition from individual domain lists into a unified domainlist (see core PR #3015).
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- src/database/gravity-db.h
+++ src/database/gravity-db.h
@@ -12,6 +12,10 @@
 
 // Table indices
 enum { GRAVITY_TABLE, EXACT_BLACKLIST_TABLE, EXACT_WHITELIST_TABLE, REGEX_BLACKLIST_TABLE, REGEX_WHITELIST_TABLE, UNKNOWN_TABLE };
+enum { GRAVITY_DOMAINLIST_EXACT_WHITELIST = 0,
+       GRAVITY_DOMAINLIST_EXACT_BLACKLIST = 1,
+       GRAVITY_DOMAINLIST_REGEX_WHITELIST = 2,
+       GRAVITY_DOMAINLIST_REGEX_BLACKLIST = 3 };
 
 bool gravityDB_open(void);
 void gravityDB_close(void);
@@ -22,7 +26,7 @@ int gravityDB_count(unsigned char list);
 bool in_whitelist(const char *domain);
 bool in_auditlist(const char *domain);
 
-bool gravityDB_addToTable(const char *table, const char* domain);
-bool gravityDB_delFromTable(const char *table, const char* domain);
+bool gravityDB_addToTable(const int type, const char* domain);
+bool gravityDB_delFromTable(const int type, const char* domain);
 
 #endif //GRAVITY_H
